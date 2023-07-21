c7ab0d542056e713c9bfdfbcd9dc1c80dfa93b5d
AvaAvarai <alice.williams.tech@gmail.com>
2023-07-20 18:58:10 -0700

    background image

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 TITLE = 'Breaker'
 WIDTH = 480
@@ -7,6 +8,8 @@ HEIGHT = 640
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
+
+background_image = pygame.image.load('digital-texture-blue.jpg')
 
 ball_image = pygame.image.load('red_ball.png')
 pygame.display.set_icon(ball_image)
@@ -22,13 +25,14 @@ ball_step = 300
 score = 0
 lives = 3
 
-player_pos = pygame.rect.Rect(WIDTH / 2, HEIGHT - 50, 38, 8)
+player_pos = pygame.rect.Rect(WIDTH / 2, HEIGHT - 50, 68, 8)
 ball_pos = pygame.rect.Rect(WIDTH / 2, HEIGHT / 2, 8, 8)
 
 ball_dir_x = 1
 ball_dir_y = 1
 ball_ang = 0
 
+blocks_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 blocks = []
 x = 5
 y = 25
@@ -45,6 +49,7 @@ while running:
             running = False
 
     screen.fill("grey")
+    screen.blit(background_image, (0, 0))
     
     for block in blocks:
         if ball_pos.colliderect(block[0], block[1], 35, 10):
@@ -60,12 +65,13 @@ while running:
                 y += 25
                 x = 5
             score += 250
+            blocks_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
             
     for block in blocks:
         pygame.draw.rect(screen, "black", (block[0], block[1], 35, 10), 35)
-        pygame.draw.rect(screen, "pink", (block[0], block[1], 32, 8), 35)
+        pygame.draw.rect(screen, blocks_color, (block[0], block[1], 32, 8), 35)
         
-    pygame.draw.rect(screen, "black", (player_pos.x, player_pos.y, 40, 10), 40)
+    pygame.draw.rect(screen, "black", (player_pos.x, player_pos.y, 70, 10), 40)
     pygame.draw.rect(screen, "orange", player_pos, 38)
     
     screen.blit(ball_image, ball_pos)
@@ -93,6 +99,7 @@ while running:
                     blocks.append((x, y))
                 y += 25
                 x = 5
+            blocks_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
     
     if ball_pos.centerx <= 0:
         ball_dir_x *= -1
@@ -105,9 +112,9 @@ while running:
     elif ball_pos.colliderect(player_pos):
         ball_dir_y *= -1
         if ball_pos.centerx < player_pos.centerx:
-            ball_ang -= abs(ball_pos.x - player_pos.x)
+            ball_ang -= dt * abs(ball_pos.centerx - player_pos.centerx)
         elif ball_pos.centerx > player_pos.centerx:
-            ball_ang += abs(ball_pos.x - player_pos.x)
+            ball_ang += dt * abs(ball_pos.centerx - player_pos.centerx)
     
     ball_pos.centery += ball_step * dt * ball_dir_y
     ball_pos.centerx += ball_ang * dt * ball_dir_x
