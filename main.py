9ffa987ae51abf1dc129b8cd19ebe5b22e22e5cd
AvaAvarai <alice.williams.tech@gmail.com>
2023-07-24 12:58:48 -0700

    Refactor

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import pygame
 import pickle
 import random
 
-FPS = 60
-WIDTH = 480
-HEIGHT = 640
+FPS_TARGET = 60
+WIN_WIDTH = 480
+WIN_HEIGHT = 640
 UI_HEIGHT = 45
 
-def reset_player_pos() -> pygame.rect.Rect:
-    return pygame.rect.Rect(WIDTH * 0.5 - 30, HEIGHT - 55, 60, 15)
+def default_player_pos() -> pygame.rect.Rect:
+    return pygame.rect.Rect(WIN_WIDTH * 0.5 - 30, WIN_HEIGHT - 55, 60, 15)
 
-def reset_boll_pos() -> pygame.rect.Rect:
-    return pygame.rect.Rect(WIDTH * 0.5 - 4, HEIGHT * 0.5, 8, 8)
+def default_ball_pos() -> pygame.rect.Rect:
+    return pygame.rect.Rect(WIN_WIDTH * 0.5 - 4, WIN_HEIGHT * 0.5, 8, 8)
 
 def color_dist(color1: tuple[int, int, int], color2: tuple[int, int, int]) -> int:
     distance: int = 0
@@ -34,7 +34,7 @@ def gen_colors(n: int) -> list[tuple[int, int, int]]:
             colors.append(new_color)
     return colors[1::]
 
-def create_bricks(level_number: int) -> list:
+def create_bricks(level_number: int) -> list[list]:
     colors: list[tuple[int, int, int]] = gen_colors(6)
     level: list[list] = []
     x: int = 7
@@ -54,186 +54,190 @@ def create_bricks(level_number: int) -> list:
         x = 7
     return level
 
-def start_game() -> None:
-
-    pygame.init()
-    clock: pygame.time.Clock = pygame.time.Clock()
-    running: bool = True
-    
-    screen: pygame.surface.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption('Breaker')
-
-    background_image = pygame.image.load('assets/image/background.jpg')
-    frame_image = pygame.image.load('assets/image/frame.png')
-    ball_image = pygame.image.load('assets/image/ball.png')
-    paddle_image = pygame.image.load('assets/image/paddle.png')
-    paddle_icon_image = pygame.image.load('assets/image/paddle_icon.png')
-    pygame.display.set_icon(pygame.image.load('assets/image/ball.ico'))
-
-    pygame.mixer.music.load('assets/audio/level' + str(1) + '.ogg')
+def play_level_music(level_number: int) -> None:
+    pygame.mixer.music.load('assets/audio/level' + str(((level_number-1) % 5) + 1) + '.ogg')
     pygame.mixer.music.play(-1)
 
-    ball_dir_x = 1
-    ball_dir_y = 1
-    ball_ang = 0
-
-    fg_font: pygame.font.Font = pygame.font.SysFont(pygame.font.match_font("cascadiamonoregular"), 32)
-    fg_font_color1 = (255, 0, 0)
-    fg_font_color2 = (255, 255, 255)
-
-    dt = 0
-    player_step = 300
-    ball_step = 350
-
-    level: int = 1
-    score: int = 0
-    bonus: int = 1
-    lives: int = 3
-
-    try:
-        file = open('highscore', 'rb')
-        highscore: int = pickle.load(file)['highscore']
-        file.close()
-    except:
-        highscore: int = 5000
-        file = open('highscore', 'wb')
-        save = {'highscore': highscore}
-        pickle.dump(save, file)
-        file.close()
-    
-    player_pos = reset_player_pos()
-    ball_pos = reset_boll_pos()
-    bricks = create_bricks(1)
-
-    while running:
-        # --- CHECK EXIT ---
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        # --- INTERFACE DRAW ---
-        screen.fill((0, 0, 0))
-        screen.blit(background_image, (5, UI_HEIGHT + 5))
-
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, WIDTH, UI_HEIGHT)) # UI
-
-        lives_text = fg_font.render("SCORE", True, fg_font_color1)
-        screen.blit(lives_text, (WIDTH - 3 * WIDTH / 4 - len("SCORE") / 2, 5))
-        lives_text = fg_font.render(str(score), True, fg_font_color2)
-        screen.blit(lives_text, (WIDTH - 3 * WIDTH / 4 + 25, 25))
+class game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.clock: pygame.time.Clock = pygame.time.Clock()
+        self.running: bool = True
         
-        score_text = fg_font.render("HIGH SCORE", True, fg_font_color1)
-        screen.blit(score_text, (WIDTH - 2 * WIDTH / 4 - len("HIGH SCORE") / 2, 5))
-        score_number_text = fg_font.render(str(highscore), True, fg_font_color2)
-        screen.blit(score_number_text, (WIDTH - 2 * WIDTH / 4 + 25, 25))
+        self.screen: pygame.surface.Surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        pygame.display.set_caption('Breaker')
+        pygame.display.set_icon(pygame.image.load('assets/image/ball.ico'))
         
-        # --- BRICK COLLIDE ---
-        for block in bricks:
-            if ball_pos.colliderect(block[0], block[1], 36, 15):
-                pygame.mixer.Sound.play(pygame.mixer.Sound("assets/audio/ping.wav"))
-                if block[3] == 1:
-                    bricks.remove(block)
-                else:
-                    block[3] -= 1
-                score += 10 * level
-                ball_dir_y *= -1
-                if len(bricks) == 0: # new level
-                    level += 1
-                    pygame.mixer.music.load('assets/audio/level' + str(level % 5) + '.ogg')
-                    pygame.mixer.music.play(-1)
-                    bricks = create_bricks(level)
-                    score += 250 * level
-                    player_pos = reset_player_pos()
-                    ball_pos = reset_boll_pos()
-                    ball_dir_x = 1
-                    ball_dir_y = 1
-                    ball_ang = 0
-                if score > highscore:
-                    highscore = score
-                    file = open('highscore', 'wb')
-                    save = {'highscore': highscore}
-                    pickle.dump(save, file)
-                    file.close()
-                if score >= 2500 * bonus:
-                    lives += 1
-                    bonus += 1
-
-        # --- BRICK DRAW ---
-        for block in bricks:
-            brick_color = block[2]
-            if block[3] == 1:
-                pygame.draw.rect(screen, "black", (block[0]-1, block[1]-1, 36, 17))
-                pygame.draw.rect(screen, brick_color, (block[0], block[1], 34, 15))
-            elif block[3] == 2:
-                pygame.draw.rect(screen, "white", (block[0]-2, block[1]-2, 36, 17))
-                pygame.draw.rect(screen, "black", (block[0]-1, block[1]-1, 36, 17))
-                pygame.draw.rect(screen, brick_color, (block[0], block[1], 34, 14))
-
-        # --- PLAYER/BALL DRAW ---
-        screen.blit(ball_image, ball_pos)
-        screen.blit(paddle_image, player_pos)
+        self.background_image = pygame.image.load('assets/image/background.jpg')
+        self.frame_image = pygame.image.load('assets/image/frame.png')
+        self.ball_image = pygame.image.load('assets/image/ball.png')
+        self.paddle_image = pygame.image.load('assets/image/paddle.png')
+        self.paddle_icon_image = pygame.image.load('assets/image/paddle_icon.png')
         
-        for life_number in range(lives):
-            screen.blit(paddle_icon_image, (10 + life_number * 35, HEIGHT - 15, 30, 8))
+        play_level_music(1)
 
-        # --- BALL PHYSICS ---
-        if ball_pos.centery > HEIGHT: # dead
-            if lives > 0:
-                pygame.mixer.Sound.play(pygame.mixer.Sound("assets/audio/drop.wav"))
-                lives -= 1
-            else:
-                pygame.mixer.Sound.play(pygame.mixer.Sound("assets/audio/crash.wav"))
-                lives = 3
-                score = 0
-                bricks = create_bricks(1)
+        self.ball_dir_x: int = 1
+        self.ball_dir_y: int = 1
+        self.ball_ang = 0
+
+        self.fg_font: pygame.font.Font = pygame.font.SysFont(pygame.font.match_font("cascadiamonoregular"), 32)
+        self.fg_font_color1: tuple[int, int, int] = (255, 0, 0)
+        self.fg_font_color2: tuple[int, int, int] = (255, 255, 255)
+
+        self.dt = 0
+        self.player_step: int = 300
+        self.ball_step: int = 350
+
+        self.level: int = 1
+        self.score: int = 0
+        self.bonus: int = 1
+        self.lives: int = 3
+
+        try: # existing file
+            file = open('highscore', 'rb')
+            self.highscore: int = pickle.load(file)['highscore']
+            file.close()
+        except: # new file
+            self.highscore: int = 5000
+            file = open('highscore', 'wb')
+            save: dict = {'highscore': self.highscore}
+            pickle.dump(save, file)
+            file.close()
+        
+        self.player_pos: pygame.rect.Rect = default_player_pos()
+        self.ball_pos: pygame.rect.Rect = default_ball_pos()
+        self.bricks: list[list] = create_bricks(1)
+
+    def start(self):
+        while self.running:
+            # --- CHECK EXIT ---
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # --- INTERFACE DRAW ---
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.background_image, (5, UI_HEIGHT + 5))
+
+            pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(0, 0, WIN_WIDTH, UI_HEIGHT)) # UI
+
+            lives_text = self.fg_font.render("SCORE", True, self.fg_font_color1)
+            self.screen.blit(lives_text, (WIN_WIDTH - 3 * WIN_WIDTH / 4 - len("SCORE") / 2, 5))
+            lives_text = self.fg_font.render(str(self.score), True, self.fg_font_color2)
+            self.screen.blit(lives_text, (WIN_WIDTH - 3 * WIN_WIDTH / 4 + 25, 25))
             
-            player_pos = reset_player_pos()
-            ball_pos = reset_boll_pos()
-            ball_dir_x = 1
-            ball_dir_y = 1
-            ball_ang = 0
+            score_text = self.fg_font.render("HIGH SCORE", True, self.fg_font_color1)
+            self.screen.blit(score_text, (WIN_WIDTH - 2 * WIN_WIDTH / 4 - len("HIGH SCORE") / 2, 5))
+            score_number_text = self.fg_font.render(str(self.highscore), True, self.fg_font_color2)
+            self.screen.blit(score_number_text, (WIN_WIDTH - 2 * WIN_WIDTH / 4 + 25, 25))
+            
+            # --- BRICK COLLIDE ---
+            for block in self.bricks:
+                if self.ball_pos.colliderect(block[0], block[1], 36, 15):
+                    pygame.mixer.Sound.play(pygame.mixer.Sound("assets/audio/ping.wav"))
+                    if block[3] == 1:
+                        self.bricks.remove(block)
+                    else:
+                        block[3] -= 1
+                    self.score += 10 * self.level
+                    self.ball_dir_y *= -1
+                    if len(self.bricks) == 0: # new level
+                        self.level += 1
+                        play_level_music(self.level)
+                        self.bricks = create_bricks(self.level)
+                        self.score += 250 * self.level
+                        self.player_pos = default_player_pos()
+                        self.ball_pos = default_ball_pos()
+                        self.ball_dir_x = 1
+                        self.ball_dir_y = 1
+                        self.ball_ang = 0
+                    if self.score > self.highscore: # new highscore
+                        highscore = self.score
+                        file = open('highscore', 'wb')
+                        save = {'highscore': highscore}
+                        pickle.dump(save, file)
+                        file.close()
+                    if self.score >= 2500 * self.bonus: # life bonus
+                        self.lives += 1
+                        self.bonus += 1
 
-        if ball_pos.centerx < 15:
-            ball_dir_x *= -1
-        elif ball_pos.centerx > WIDTH - 15:
-            ball_dir_x *= -1
-        
-        if ball_pos.centery < UI_HEIGHT + 10:
-            ball_dir_y *= -1
-        
-        # --- BALL/PLAYER COLLIDE ---
-        if ball_pos.colliderect(player_pos):
-            ball_dir_y = -1
-            ball_dir_x = 1
-            if ball_pos.centerx < player_pos.centerx:
-                ball_ang = -(player_pos.centerx - ball_pos.centerx)
-            elif ball_pos.centerx > player_pos.centerx:
-                ball_ang = (ball_pos.centerx - player_pos.centerx)
+            # --- BRICK DRAW ---
+            for block in self.bricks:
+                brick_color = block[2]
+                if block[3] == 1:
+                    pygame.draw.rect(self.screen, "black", (block[0]-1, block[1]-1, 36, 17))
+                    pygame.draw.rect(self.screen, brick_color, (block[0], block[1], 34, 15))
+                elif block[3] == 2:
+                    pygame.draw.rect(self.screen, "white", (block[0]-2, block[1]-2, 36, 17))
+                    pygame.draw.rect(self.screen, "black", (block[0]-1, block[1]-1, 36, 17))
+                    pygame.draw.rect(self.screen, brick_color, (block[0], block[1], 34, 14))
+
+            # --- PLAYER/BALL DRAW ---
+            self.screen.blit(self.ball_image, self.ball_pos)
+            self.screen.blit(self.paddle_image, self.player_pos)
+            
+            for life_number in range(self.lives):
+                self.screen.blit(self.paddle_icon_image, (10 + life_number * 35, WIN_HEIGHT - 15, 30, 8))
+
+            # --- BALL PHYSICS ---
+            if self.ball_pos.centery > WIN_HEIGHT: # dead
+                if self.lives > 0:
+                    pygame.mixer.Sound.play(pygame.mixer.Sound("assets/audio/drop.wav"))
+                    self.lives -= 1
+                else:
+                    pygame.mixer.Sound.play(pygame.mixer.Sound("assets/audio/crash.wav"))
+                    self.lives = 3
+                    self.score = 0
+                    self.bricks = create_bricks(1)
                 
-        # --- BALL UPDATE ---
-        y = ball_step * dt * ball_dir_y
-        x = 5 * ball_ang * dt * ball_dir_x
-        ball_pos = ball_pos.move(x, y)
-        
-        # --- INPUT ---
-        keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_a] or keys[pygame.K_w] or keys[pygame.K_LEFT] or keys[pygame.K_UP]: # left
-            if player_pos.right - 5 - (player_step * dt) >= 0:
-                player_pos = player_pos.move((-1 * player_step * dt, 0))
-        elif keys[pygame.K_d] or keys[pygame.K_s] or keys[pygame.K_RIGHT] or keys[pygame.K_DOWN]: # right
-            if player_pos.left + 5 + (player_step * dt) <= WIDTH:
-                player_pos = player_pos.move((player_step * dt, 0))
-        elif keys[pygame.K_ESCAPE]:
-            running = False
-        
-        # --- DRAW FRAME ---
-        screen.blit(frame_image, (0, UI_HEIGHT))
-        
-        # --- END FRAME ---
-        pygame.display.flip() # bufferswap
-        dt = clock.tick(FPS) / 1000 # limit fps
+                self.player_pos = default_player_pos()
+                self.ball_pos = default_ball_pos()
+                self.ball_dir_x = 1
+                self.ball_dir_y = 1
+                self.ball_ang = 0
+
+            if self.ball_pos.centerx < 15:
+                self.ball_dir_x *= -1
+            elif self.ball_pos.centerx > WIN_WIDTH - 15:
+                self.ball_dir_x *= -1
+            
+            if self.ball_pos.centery < UI_HEIGHT + 10:
+                self.ball_dir_y *= -1
+            
+            # --- BALL/PLAYER COLLIDE ---
+            if self.ball_pos.colliderect(self.player_pos):
+                self.ball_dir_y = -1
+                self.ball_dir_x = 1
+                if self.ball_pos.centerx < self.player_pos.centerx:
+                    self.ball_ang = -(self.player_pos.centerx - self.ball_pos.centerx)
+                elif self.ball_pos.centerx > self.player_pos.centerx:
+                    self.ball_ang = (self.ball_pos.centerx - self.player_pos.centerx)
+                    
+            # --- BALL UPDATE ---
+            y = self.ball_step * self.dt * self.ball_dir_y
+            x = 5 * self.ball_ang * self.dt * self.ball_dir_x
+            self.ball_pos = self.ball_pos.move(x, y)
+            
+            # --- INPUT ---
+            keys = pygame.key.get_pressed()
+            
+            if keys[pygame.K_a] or keys[pygame.K_w] or keys[pygame.K_LEFT] or keys[pygame.K_UP]: # left
+                if self.player_pos.right - 5 - (self.player_step * self.dt) >= 0:
+                    self.player_pos = self.player_pos.move((-1 * self.player_step * self.dt, 0))
+            elif keys[pygame.K_d] or keys[pygame.K_s] or keys[pygame.K_RIGHT] or keys[pygame.K_DOWN]: # right
+                if self.player_pos.left + 5 + (self.player_step * self.dt) <= WIN_WIDTH:
+                    self.player_pos = self.player_pos.move((self.player_step * self.dt, 0))
+            elif keys[pygame.K_ESCAPE]:
+                self.running = False
+            
+            # --- DRAW FRAME ---
+            self.screen.blit(self.frame_image, (0, UI_HEIGHT))
+            
+            # --- END FRAME ---
+            pygame.display.flip() # bufferswap
+            self.dt = self.clock.tick(FPS_TARGET) / 1000 # limit fps
 
 if __name__ == "__main__":
-    start_game()
+    new_game = game()
+    new_game.start()
     pygame.quit()
